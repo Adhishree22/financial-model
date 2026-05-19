18bb1bae88413d663a6b1a8a6dd2ea6cb9134bf7
Adhishree22 <adhishree2207@gmail.com>
2026-05-19 09:36:29 +0000

    Added

--- analysis/scoring/risk.py
+++ analysis/scoring/risk.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 def build_risk_score(ratios_df, growth_df):
-  
+
   hist = ratios_df[ratios_df.index <= 2025]
   risk_df = pd.DataFrame(index=ratios_df.index)
 
@@ -18,7 +18,7 @@ def build_risk_score(ratios_df, growth_df):
       1 / ratios_df["InterestCoverage"].replace(0, np.nan),
       1 / hist["InterestCoverage"].replace(0, np.nan)
   )
-  
+
   risk_df["Liquidity_Risk"] = (
       0.5 * normalize(1 / ratios_df["CurrentRatio"], 1 / hist["CurrentRatio"]) +
       0.5 * normalize(1 / ratios_df["QuickRatio"], 1 / hist["QuickRatio"])
@@ -28,8 +28,10 @@ def build_risk_score(ratios_df, growth_df):
 
   risk_df["CashFlow_Risk"] = normalize(1 - ratios_df["FCF_Conversion"],1 - hist["FCF_Conversion"])
 
-  risk_df["Operating_Risk"] = normalize(ratios_df["Cost_Rigidity"], hist["Cost_Rigidity"])
-
+  risk_df["Operating_Risk"] = (
+    0.5 * normalize(ratios_df["Cost_Rigidity"], hist["Cost_Rigidity"]) +
+    0.5 * normalize(ratios_df["Operating_Leverage"], hist["Operating_Leverage"])
+  )
   vol = growth_df["Revenue_Growth%"].rolling(3).std()
   hist_vol = vol.loc[vol.index <= 2025]
 
@@ -63,7 +65,7 @@ def build_risk_score(ratios_df, growth_df):
 
   risk_df["Safety_Score"] = 100 - risk_df["Risk_Score"]
   risk_df["Safety_Score"] = risk_df["Safety_Score"].clip(0, 100)
-  
+
   risk_df = risk_df.round(2)
-  
+
   return risk_df
